438d53affe43239203472cf6366382f0110c3190
Riddhika1112 <20051158@kiit.ac.in>
2023-01-27 10:31:06 +0530

    First commit.

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Document Oriented Model",
+            "Development Oriented Model",
+            "Document Object Model",
+            "Development Object Model"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which is NOT a way to reduce the page loading time?",
+        answer: "Increasing the image size.",
+        options: [
+            "Reduce the image size.",
+            "Clean the web code.",
+            "Remove unneccessary widgets.",
+            "Increasing the image size."
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylesheet Query Language",
+            "Structured Query Language",
+            "Statement Query Language",
+            "Stylish Question Language"
+        ]
+    },
+];
